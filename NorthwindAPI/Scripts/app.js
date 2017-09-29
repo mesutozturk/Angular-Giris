75af598a67f20278140dd11ef2d062e86b9f9943
mesutozturk <mesutoztrk@msn.com>
2017-09-29 12:02:10 +0300

    a

--- NorthwindAPI/Scripts/app.js
+++ NorthwindAPI/Scripts/app.js
@@ -1,6 +1,41 @@
 ﻿
 var app = angular.module("NorthApp", []);
 
-app.controller("CategoryCtrl", function ($scope) {
+app.service("api", function ($http) {
+    var apiUrl = "http://localhost:65408/category/";
+    return {
+        getAllCategories: function (success) {
+            $http({
+                url: apiUrl + 'getallcategories',
+                method: 'GET',
+                dataType: 'JSON'
+            }).then(function (response) {
+                success(response.data);
+            });
+        },
+        getCategoryById: function (id, success) {
+            $http({
+                url: apiUrl + 'GetCategoryById/' + id,
+                method: 'GET',
+                dataType: 'JSON'
+            }).then(function (response) {
+                success(response.data);
+            });
+        }
+    }
+});
 
+app.controller("CategoryCtrl", function ($scope, api) {
+    $scope.categories = [];
+
+    function init() {
+        api.getAllCategories(function (response) {
+            if (response.success)
+                $scope.categories = response.data;
+            else
+                alert(response.message);
+        });
+    }
+
+    init();
 });
